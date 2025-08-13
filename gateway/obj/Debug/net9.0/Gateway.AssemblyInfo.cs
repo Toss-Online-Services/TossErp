28675d6b78ddc027acb27651c9d9c65df9edbd89
gontsemoses <moses.gontse@tossonline.co.za>
2025-08-13 11:17:18 +0200

    Update Gateway project binaries and assembly information; mark subproject as dirty and update source link version.

--- gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
+++ gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e96748bd3827025082f53ee8d1e8ffa871daf6dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca4f54c14ae78891744931d234da2a570a33a3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
