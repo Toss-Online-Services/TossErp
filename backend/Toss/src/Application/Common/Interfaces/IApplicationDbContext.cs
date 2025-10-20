847ee90ba36ea9826b8e03e56ca1e83b7a39ed1a
gontsemoses <moses.gontse@tossonline.co.za>
2025-10-20 21:29:55 +0200

    Add functional and unit tests for Todo items and lists, implement PostgreSQL test database setup
    - Implemented PostgreSQLTestcontainersTestDatabase for functional tests using Testcontainers.
    - Created TestDatabaseFactory to initialize the test database.
    - Developed Testing class for shared test setup and utility methods.
    - Added Create, Update, and Delete tests for Todo items and lists.
    - Implemented validation tests for Todo item and list commands.
    - Added integration tests for authentication flow in the web application.
    - Created unit tests for common behaviors, exceptions, and mappings.
    - Updated appsettings.json for test database connection.
    - Added project files for unit and integration tests.

---
+++ backend/Toss/src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Toss.Domain.Entities;
+
+namespace Toss.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<TodoList> TodoLists { get; }
+
+    DbSet<TodoItem> TodoItems { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ backend/Toss/src/Domain/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Toss.Domain.Common;
+global using Toss.Domain.Entities;
+global using Toss.Domain.Enums;
+global using Toss.Domain.Events;
+global using Toss.Domain.Exceptions;
+global using Toss.Domain.ValueObjects;
